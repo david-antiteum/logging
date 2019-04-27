7c3d388c2a24b4b8e39a7e46b84190612d69fc75
David Garcia <david.garcia@antiteum.com>
2019-04-27 14:38:49 +0200

    Ledaer detection

--- utils/consul_client.h
+++ utils/consul_client.h
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <optional>
+#include <thread>
+#include <atomic>
 
 #include <cpprest/http_client.h>
 #include <cpprest/json.h>
@@ -24,7 +26,7 @@ private:
 	const std::string mConsulAddress{ "http://127.0.0.1:8500/v1" };
 
 public:
-	std::optional<Service> get( const std::string & id )
+	std::optional<Service> get( const std::string & id ) const
 	{
 		std::optional<Service>			res;
 		const std::string 				query = fmt::format( "{}/agent/service/{}", mConsulAddress, id );
@@ -57,7 +59,7 @@ public:
 		return res;
 	}
 
-	void add( const std::string & name, int port, const std::string & group )
+	void add( const std::string & name, int port, const std::string & group ) const
 	{
 		const std::string 				query = fmt::format( "{}/agent/service/register", mConsulAddress );
 		web::http::client::http_client 	client( utility::conversions::to_string_t( query ));
@@ -98,7 +100,7 @@ public:
 		.wait();
 	}
 
-	void remove( const std::string & id, const std::string & group )
+	void remove( const std::string & id, const std::string & group ) const
 	{
 		const std::string 				query = fmt::format( "{}/agent/service/deregister/{}_{}", mConsulAddress, id, group );
 		web::http::client::http_client 	client( utility::conversions::to_string_t( query ));
@@ -193,7 +195,7 @@ public:
 		Error
 	};
 
-	Status acquire( const std::string & service, const Session & session )
+	Status acquire( const std::string & service, const Session & session ) const
 	{
 		Status							res = Status::Error;
 		const std::string 				query = fmt::format( "{}/kv/service/{}/leader?acquire={}", mConsulAddress, service, session );
@@ -223,7 +225,7 @@ public:
 		return res;
 	}
 
-	void release( const std::string & service, const Session & session )
+	void release( const std::string & service, const Session & session ) const
 	{
 		const std::string 				query = fmt::format( "{}/kv/service/{}/leader?release={}", mConsulAddress, service, session );
 		web::http::client::http_client 	client( utility::conversions::to_string_t( query ));
@@ -243,6 +245,91 @@ public:
 			}
 		})
 		.wait();
+	}
+};
+
+class Observer
+{
+private:
+	const std::string mConsulAddress{ "http://127.0.0.1:8500/v1" };
+
+public:
+	Observer( const std::string & service, const Session & session )
+		: mService( service )
+		, mSession( session )
+	{
+
+	}
+
+	~Observer()
+	{
+		if( mThread ){
+			mRunThread = 0;
+			mThread->join();
+		}
+	}
+
+	void leader( std::function<void(Session)> leaderObserver )
+	{
+		mLeaderObserver = leaderObserver;
+	}
+
+	void run()
+	{
+		mRunThread = 1;
+		mThread = std::make_unique<std::thread>( &Observer::realRun, this );
+	}
+
+private:
+	std::string						mService;
+	Session							mSession;
+	std::function<void(Session)> 	mLeaderObserver;
+
+	std::unique_ptr<std::thread>	mThread;
+	std::atomic<short> 				mRunThread;
+
+	void realRun()
+	{
+		using namespace std::chrono_literals;
+
+		while( mRunThread > 0 ){
+			if( mLeaderObserver ){
+				const std::string 				query = fmt::format( "{}/kv/service/{}/leader", mConsulAddress, mService );
+				web::http::client::http_client 	client( utility::conversions::to_string_t( query ));
+				web::http::http_request			req( web::http::methods::GET );
+
+				req.headers().set_content_type( U("application/json; charset=utf-8") );
+				client.request( req ).then([]( web::http::http_response response ){
+					if( response.status_code() == web::http::status_codes::OK ){
+						return response.extract_json();
+					}
+					return pplx::task_from_result( web::json::value() );
+				}).then([ this ](pplx::task<web::json::value> previousTask){
+					try{
+						const auto jsonRes = previousTask.get();
+						Session session;
+
+						if( jsonRes.is_array() ){
+							const auto jsonArray = jsonRes.as_array();
+							if( jsonArray.size() > 0 ){
+								const auto leaderInfo = jsonArray.at(0);
+
+								if( leaderInfo.has_field( utility::conversions::to_string_t( "Session" )) ){
+									session = leaderInfo.at( utility::conversions::to_string_t( "Session" )).as_string();
+								}
+							}
+						}
+						if( session != mSession ){
+							mLeaderObserver( session );
+						}
+					}catch( web::http::http_exception const & e ){
+						std::wcout << e.what() << std::endl;
+					}
+				})
+				.wait();
+			}
+			std::this_thread::sleep_for( 2s );
+		}
 	}
 };
 
